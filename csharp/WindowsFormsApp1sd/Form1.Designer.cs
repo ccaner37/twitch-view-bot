de64275cc55ec897ed939171a0ee32ced37bd752
ccaner37 <ccaner37@hotmail.com>
2021-04-26 16:14:52 +0300

    uploaded both versions

---
+++ csharp/WindowsFormsApp1sd/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1sd
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(283, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(172, 251);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 278);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
+
---
+++ csharp/WindowsFormsApp1sd/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Leaf.xNet;
+using OpenQA.Selenium.IE;
+
+namespace WindowsFormsApp1sd
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			Thread newThread = new Thread(PingIP);
+			newThread.Start();
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void PingIP()
+		{
+
+			/*
+			ChromeOptions proxy = new ChromeOptions();
+			proxy.AddArguments("--proxy-server=" + textBox1.Text);
+			IWebDriver driver1 = new ChromeDriver(proxy);
+			driver1.Navigate().GoToUrl("https://www.twitch.tv/test");
+
+			System.Threading.Thread.Sleep(10000);
+
+			((IJavaScriptExecutor)driver1).ExecuteScript("window.open();");
+			driver1.SwitchTo().Window(driver1.WindowHandles.Last());
+			*/
+
+			
+			//foreach (var item in listBox1.Items)
+			//{
+				//var request2 = item.ToString();
+
+
+					ChromeOptions proxy = new ChromeOptions();
+					proxy.AddArguments("--proxy-server=" + textBox1.Text);
+					IWebDriver driver1 = new ChromeDriver(proxy);
+					driver1.Navigate().GoToUrl("https://www.twitch.tv/ChannelName");
+
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			listBox1.Text = File.ReadAllText(@"ro.txt").ToString();
+			listBox1.Items.AddRange(listBox1.Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None));
+		}
+
+
+	}
+}
